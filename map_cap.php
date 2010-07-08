5c609c7ff9614620dbbfc874c0589123b4d978f5
Brent Shepherd <thenbrent@gmail.com>
2010-07-09 00:36:37 +1000

    The woods are lovely, dark, and deep, But I have promises to keep, And miles to go before I sleep.

--- map_cap.php
+++ map_cap.php
@@ -23,7 +23,9 @@ add_action( 'admin_menu', 'mc_add_admin_menu' );
  * Allow site admin to choose which roles can do what to marketplace posts.
  */
 function mc_capabilities_settings_page() { 
-	global $wp_roles, $market_system;
+	global $wp_roles;
+
+	$message = do_action( 'mc_capabilities_settings_page' );
 
 	$role_names = $wp_roles->get_names();
 	$roles = array();
@@ -33,53 +35,62 @@ function mc_capabilities_settings_page() {
 		$roles[ $key ]->display_name = $value;
 	}
 
-	$post_types = get_post_types( array( 'public'   => true, '_builtin' => false ), 'names', 'and'); 
+	$post_types = get_post_types( array( 'public'   => true, '_builtin' => false ), 'objects' ); 
+	error_log('$post_types = ' . print_r( $post_types, true ));
 	
-	wp_nonce_field( 'pp_capabilities_settings' );  ?>
-	<div class="wrap buggypress-settings">
-	<div class="prospress-capabilities">
-	<?php
-	foreach( $post_types as $post_type ) {
 	?>
-			<h3><?php printf( __( '%s Capabilities', 'map-cap' ), ucfirst( $post_type ) ); ?></h3>
-			<div class="map-cap">
-				<h4><?php printf( __( "Publish %s", 'map-cap' ), $market_system->display_name() ); ?></h4>
-				<?php foreach ( $roles as $role ): if( $role->name == 'administrator' ) continue; ?>
-				<label for="<?php echo $role->name; ?>-publish">
-					<input type="checkbox" id="<?php echo $role->name; ?>-publish" name="<?php echo $role->name; ?>-publish"<?php checked( $role->capabilities[ 'publish_prospress_posts' ], 1 ); ?> />
-					<?php echo $role->display_name; ?>
-				</label>
-				<?php endforeach; ?>
-			</div>
-			<div class="map-cap">
-				<h4><?php printf( __( "Edit own %s", 'map-cap' ), $market_system->display_name() ); ?></h4>
-				<?php foreach ( $roles as $role ): if( $role->name == 'administrator' ) continue; ?>
-				<label for="<?php echo $role->name; ?>-edit">
-				  	<input type="checkbox" id="<?php echo $role->name; ?>-edit" name="<?php echo $role->name; ?>-edit"<?php checked( $role->capabilities[ 'edit_published_prospress_posts' ], 1 ); ?> />
-					<?php echo $role->display_name; ?>
-				</label>
-				<?php endforeach; ?>
-			</div>
-			<div class="map-cap">
-				<h4><?php printf( __( "Edit others' %s", 'map-cap' ), $market_system->display_name() ); ?></h4>
-				<?php foreach ( $roles as $role ): if( $role->name == 'administrator' ) continue; ?>
-				<label for="<?php echo $role->name; ?>-edit-others">
-					<input type="checkbox" id="<?php echo $role->name; ?>-edit-others" name="<?php echo $role->name; ?>-edit-others"<?php checked( $role->capabilities[ 'edit_others_prospress_posts' ], 1 ); ?> />
-					<?php echo $role->display_name; ?>
-				</label>
-				<?php endforeach; ?>
-			</div>
-			<div class="map-cap">
-				<h4><?php printf( __( "View private %s", 'map-cap' ), $market_system->display_name() ); ?></h4>
-				<?php foreach ( $roles as $role ): if( $role->name == 'administrator' ) continue; ?>
-				<label for="<?php echo $role->name; ?>-private">
-					<input type="checkbox" id="<?php echo $role->name; ?>-private" name="<?php echo $role->name; ?>-private"<?php checked( $role->capabilities[ 'read_private_prospress_posts' ], 1 ); ?> />
-					<?php echo $role->display_name; ?>
-				</label>
-				<?php endforeach; ?>
-			</div>
+	<div class="wrap map-cap-settings">
+	  <h2><?php _e( 'Map Capabilities', 'map-cap' ); ?></h2>
+	  <?php if ( $message ) : ?>
+	    <div id="message" class="updated fade"><p><?php echo $$message; ?></p></div>
+	  <?php endif; ?>
+	  <form id="map-cap-form" method="post" action="">
+	<?php
+	foreach( $post_types as $post_type => $post_type_details ) {
+		wp_nonce_field( 'mc_capabilities_settings' );
+		?>
+		<h3><?php printf( __( '%s Capabilities', 'map-cap' ), $post_type_details->labels->singular_name ); ?></h3>
+		<div class="map-cap">
+			<h4><?php printf( __( "Publish %s", 'map-cap' ), $post_type_details->labels->name ); ?></h4>
+			<?php foreach ( $roles as $role ): ?>
+			<label for="<?php echo $post_type . '-' . $role->name; ?>-publish">
+				<input type="checkbox" id="<?php echo $post_type . '-' . $role->name; ?>-publish" name="<?php echo $post_type . '-' . $role->name; ?>-publish"<?php checked( $role->capabilities[ 'publish_' . $post_type . '_posts' ], 1 ); ?> />
+				<?php echo $role->display_name; ?>
+			</label>
+			<?php endforeach; ?>
+		</div>
+		<div class="map-cap">
+			<h4><?php printf( __( "Edit own %s", 'map-cap' ), $post_type_details->labels->name  ); ?></h4>
+			<?php foreach ( $roles as $role ): ?>
+			<label for="<?php echo $post_type . '-' . $role->name; ?>-edit">
+			  	<input type="checkbox" id="<?php echo $post_type . '-' . $role->name; ?>-edit" name="<?php echo $post_type . '-' . $role->name; ?>-edit"<?php checked( $role->capabilities[ 'edit_published_' . $post_type . '_posts' ], 1 ); ?> />
+				<?php echo $role->display_name; ?>
+			</label>
+			<?php endforeach; ?>
+		</div>
+		<div class="map-cap">
+			<h4><?php printf( __( "Edit others' %s", 'map-cap' ), $post_type_details->labels->name  ); ?></h4>
+			<?php foreach ( $roles as $role ): ?>
+			<label for="<?php echo $post_type . '-' . $role->name; ?>-edit-others">
+				<input type="checkbox" id="<?php echo $post_type . '-' . $role->name; ?>-edit-others" name="<?php echo $post_type . '-' . $role->name; ?>-edit-others"<?php checked( $role->capabilities[ 'edit_others_' . $post_type . '_posts' ], 1 ); ?> />
+				<?php echo $role->display_name; ?>
+			</label>
+			<?php endforeach; ?>
+		</div>
+		<div class="map-cap">
+			<h4><?php printf( __( "View private %s", 'map-cap' ), $post_type_details->labels->name  ); ?></h4>
+			<?php foreach ( $roles as $role ): ?>
+			<label for="<?php echo $post_type . '-' . $role->name; ?>-private">
+				<input type="checkbox" id="<?php echo $post_type . '-' . $role->name; ?>-private" name="<?php echo $post_type . '-' . $role->name; ?>-private"<?php checked( $role->capabilities[ 'read_private_' . $post_type . '_posts' ], 1 ); ?> />
+				<?php echo $role->display_name; ?>
+			</label>
+			<?php endforeach; ?>
+		</div>
 	<?php } ?>
-	</div>
+    <div class="save-settings-form" style="margin-top: 1em;">
+      <input class="button button-highlighted" type="submit" value="<?php _e( 'Save', 'map-cap' ); ?>" />
+    </div>
+	</form>
 	</div>
 	<?php
 }
@@ -90,10 +101,12 @@ function mc_capabilities_settings_page() {
  * the options table of the database, they're not added to the whitelist as is expected by this filter, instead 
  * they're added to the appropriate roles.
  */
-function mc_capabilities_whitelist( $whitelist_options ) {
-	global $wp_roles, $market_system;
+function mc_save_capabilities() {
+	global $wp_roles;
 
-    if ( $_POST['_wpnonce' ] && check_admin_referer( 'pp_capabilities_settings' ) && current_user_can( 'manage_options' ) ){
+    if ( $_POST['_wpnonce' ] && check_admin_referer( 'mc_capabilities_settings' ) && current_user_can( 'manage_options' ) ){
+
+		error_log('POST = ' . print_r( $_POST, true ) );
 
 		$role_names = $wp_roles->get_names();
 		$roles = array();
@@ -103,133 +116,139 @@ function mc_capabilities_whitelist( $whitelist_options ) {
 			$roles[ $key ]->display_name = $value;
 		}
 
+		$post_types = get_post_types( array( 'public'   => true, '_builtin' => false ), 'objects' ); 
+
 		foreach ( $roles as $key => $role ) {
 
-			if( $role->name == 'administrator' )
-				continue;
+			foreach( $post_types as $post_type => $post_type_details ) {
 
-			// Shared capability
-			if ( ( isset( $_POST[ $key . '-publish' ] )  && $_POST[ $key . '-publish' ] == 'on' ) || ( isset( $_POST[ $key . '-edit' ] )  && $_POST[ $key . '-edit' ] == 'on' ) || ( isset( $_POST[ $key . '-edit-others' ] )  && $_POST[ $key . '-edit-others' ] == 'on' ) ) {
-				$role->add_cap( 'edit_prospress_posts' );
-			} else {
-				$role->remove_cap( 'edit_prospress_posts' );
+				// Shared capability
+				if ( ( isset( $_POST[ $post_type . '-' . $key . '-publish' ] )  && $_POST[ $post_type . '-' . $key . '-publish' ] == 'on' ) || ( isset( $_POST[ $post_type . '-' . $key . '-edit' ] )  && $_POST[ $post_type . '-' . $key . '-edit' ] == 'on' ) || ( isset( $_POST[ $post_type . '-' . $key . '-edit-others' ] )  && $_POST[ $post_type . '-' . $key . '-edit-others' ] == 'on' ) ) {
+					$role->add_cap( 'edit_' . $post_type . '_posts' );
+				} else {
+					$role->remove_cap( 'edit_' . $post_type . '_posts' );
+				}
+
+				if ( isset( $_POST[ $post_type . '-' . $key . '-publish' ] )  && $_POST[ $post_type . '-' . $key . '-publish' ] == 'on' ) {
+					$role->add_cap( 'publish_' . $post_type . '_posts' );
+					$role->add_cap( 'delete_' . $post_type . '_posts' );
+				} else {
+					$role->remove_cap( 'publish_' . $post_type . '_posts' );
+					$role->remove_cap( 'delete_' . $post_type . '_posts' );
+				}
+
+				if ( ( isset( $_POST[ $post_type . '-' . $key . '-edit' ] )  && $_POST[ $post_type . '-' . $key . '-edit' ] == 'on' ) || ( isset( $_POST[ $post_type . '-' . $key . '-edit-others' ] )  && $_POST[ $post_type . '-' . $key . '-edit-others' ] == 'on' ) ) {
+					$role->add_cap( 'edit_published_' . $post_type . '_posts' );
+					$role->add_cap( 'delete_published_' . $post_type . '_posts' );
+					$role->add_cap( 'edit_private_' . $post_type . '_posts' );
+				} else {
+					$role->remove_cap( 'edit_published_' . $post_type . '_posts' );
+					$role->remove_cap( 'delete_published_' . $post_type . '_posts' );
+					$role->remove_cap( 'edit_private_' . $post_type . '_posts' );
+				}
+
+				if ( isset( $_POST[ $post_type . '-' . $key . '-edit-others' ] )  && $_POST[ $post_type . '-' . $key . '-edit-others' ] == 'on' ) {
+					$role->add_cap( 'edit_others_' . $post_type . '_posts' );
+				} else {
+					$role->remove_cap( 'edit_others_' . $post_type . '_posts' );
+		        }
+
+				if ( isset( $_POST[ $post_type . '-' . $key . '-private' ] )  && $_POST[ $post_type . '-' . $key . '-private' ] == 'on' ) {
+					$role->add_cap( 'edit_' . $post_type . '_posts' );
+					$role->add_cap( 'read_private_' . $post_type . '_posts' );
+				} else {
+					$role->remove_cap( 'read_private_' . $post_type . '_posts' );
+					$role->remove_cap( 'edit_' . $post_type . '_posts' );
+				}
+				
+				error_log( ' role = ' . print_r( $role, true ) );
 			}
-
-			if ( isset( $_POST[ $key . '-publish' ] )  && $_POST[ $key . '-publish' ] == 'on' ) {
-				$role->add_cap( 'publish_prospress_posts' );
-				$role->add_cap( 'delete_prospress_posts' );
-			} else {
-				$role->remove_cap( 'publish_prospress_posts' );
-				$role->remove_cap( 'delete_prospress_posts' );
-			}
-
-			if ( ( isset( $_POST[ $key . '-edit' ] )  && $_POST[ $key . '-edit' ] == 'on' ) || ( isset( $_POST[ $key . '-edit-others' ] )  && $_POST[ $key . '-edit-others' ] == 'on' ) ) {
-				$role->add_cap( 'edit_published_prospress_posts' );
-				$role->add_cap( 'delete_published_prospress_posts' );
-				$role->add_cap( 'edit_private_prospress_posts' );
-			} else {
-				$role->remove_cap( 'edit_published_prospress_posts' );
-				$role->remove_cap( 'delete_published_prospress_posts' );
-				$role->remove_cap( 'edit_private_prospress_posts' );
-			}
-
-			if ( isset( $_POST[ $key . '-edit-others' ] )  && $_POST[ $key . '-edit-others' ] == 'on' ) {
-				$role->add_cap( 'edit_others_prospress_posts' );
-			} else {
-				$role->remove_cap( 'edit_others_prospress_posts' );
-	        }
-
-			if ( isset( $_POST[ $key . '-private' ] )  && $_POST[ $key . '-private' ] == 'on' ) {
-				$role->add_cap( 'edit_prospress_posts' );
-				$role->add_cap( 'read_private_prospress_posts' );
-			} else {
-				$role->remove_cap( 'read_private_prospress_posts' );
-				$role->remove_cap( 'edit_prospress_posts' );
-			}
-
 		}
     }
-
-	return $whitelist_options;
+    return 'Settings saved';
 }
-add_filter( 'pp_options_whitelist', 'pp_capabilities_whitelist' );
+add_action('mc_capabilities_settings_page', 'mc_save_capabilities', 1);
 
 
 function mc_map_meta_cap( $caps, $cap, $user_id, $args ){
 
-	if( $cap == 'edit_prospress_post' ) {
+	$post_types = get_post_types( array( 'public'   => true, '_builtin' => false ), 'objects' ); 
 
-		$author_data = get_userdata( $user_id );
+	foreach( $post_types as $post_type => $post_type_details ) {
+		//error_log( "in mc_map_meta_cap, cap = $cap, post_type = $post_type" );
+		if( $cap == 'edit_' . $post_type . '_post' ) {
 
-		$post = get_post( $args[0] );
-
-		$post_type = get_post_type_object( $post->post_type );
-
-		$post_author_data = get_userdata( $post->post_author );
-
-		if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID ) {
-
-			if ( 'publish' == $post->post_status ) {
-				$caps[0] = 'edit_published_prospress_posts';
-			} elseif ( 'private' == $post->post_status ) {
-				$caps[0] = 'edit_private_prospress_posts';
-			} elseif ( 'trash' == $post->post_status ) {
-				if ('publish' == get_post_meta($post->ID, '_wp_trash_meta_status', true) )
-					$caps[0] = 'edit_published_prospress_posts';
-			} else {
-				$caps[0] = 'edit_prospress_posts';
-				$caps[] = 'publish_prospress_posts';
-			}
-		} else {
-			$caps[0] = 'edit_others_prospress_posts';
-
-			if ( 'publish' == $post->post_status )
-				$caps[] = 'edit_published_prospress_posts';
-			elseif ( 'private' == $post->post_status )
-				$caps[] = 'edit_private_prospress_posts';
-		}
-	} elseif( $cap == 'delete_prospress_post' ) {
-		$author_data = get_userdata( $user_id );
-		$post = get_post( $args[0] );
-
-		if ( '' != $post->post_author ) {
-			$post_author_data = get_userdata( $post->post_author );
-		} else {
-			//No author, default to current user
-			$post_author_data = $author_data;
-		}
-
-		if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID ) {
-
-			if ( 'publish' == $post->post_status ) {
-				$caps[0] = 'delete_published_prospress_posts';
-			} elseif ( 'trash' == $post->post_status ) {
-				if ('publish' == get_post_meta($post->ID, '_wp_trash_meta_status', true) )
-					$caps[0] = 'delete_published_prospress_posts';
-			} else {
-				$caps[0] = 'delete_prospress_posts';
-			}
-		} else {
-			$caps[0] = 'edit_others_prospress_posts';
-
-			if ( 'publish' == $post->post_status || 'private' == $post->post_status )
-				$caps[] = 'delete_published_prospress_posts';
-		}
-	} elseif( $cap == 'read_prospress_post' ) {
-		$post = get_post( $args[0] );
-
-		if ( 'private' != $post->post_status ) {
-			$caps[0] = 'read';
-		} else {
 			$author_data = get_userdata( $user_id );
+
+			$post = get_post( $args[0] );
+
+			$post_type = get_post_type_object( $post->post_type );
+
 			$post_author_data = get_userdata( $post->post_author );
-			if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID )
+
+			if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID ) {
+
+				if ( 'publish' == $post->post_status ) {
+					$caps[0] = 'edit_published_' . $post_type . '_posts';
+				} elseif ( 'private' == $post->post_status ) {
+					$caps[0] = 'edit_private_' . $post_type . '_posts';
+				} elseif ( 'trash' == $post->post_status ) {
+					if ('publish' == get_post_meta($post->ID, '_wp_trash_meta_status', true) )
+						$caps[0] = 'edit_published_' . $post_type . '_posts';
+				} else {
+					$caps[0] = 'edit_' . $post_type . '_posts';
+					$caps[] = 'publish_' . $post_type . '_posts';
+				}
+			} else {
+				$caps[0] = 'edit_others_' . $post_type . '_posts';
+
+				if ( 'publish' == $post->post_status )
+					$caps[] = 'edit_published_' . $post_type . '_posts';
+				elseif ( 'private' == $post->post_status )
+					$caps[] = 'edit_private_' . $post_type . '_posts';
+			}
+		} elseif( $cap == 'delete_' . $post_type . '_post' ) {
+			$author_data = get_userdata( $user_id );
+			$post = get_post( $args[0] );
+
+			if ( '' != $post->post_author ) {
+				$post_author_data = get_userdata( $post->post_author );
+			} else {
+				//No author, default to current user
+				$post_author_data = $author_data;
+			}
+
+			if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID ) {
+
+				if ( 'publish' == $post->post_status ) {
+					$caps[0] = 'delete_published_' . $post_type . '_posts';
+				} elseif ( 'trash' == $post->post_status ) {
+					if ('publish' == get_post_meta($post->ID, '_wp_trash_meta_status', true) )
+						$caps[0] = 'delete_published_' . $post_type . '_posts';
+				} else {
+					$caps[0] = 'delete_' . $post_type . '_posts';
+				}
+			} else {
+				$caps[0] = 'edit_others_' . $post_type . '_posts';
+
+				if ( 'publish' == $post->post_status || 'private' == $post->post_status )
+					$caps[] = 'delete_published_' . $post_type . '_posts';
+			}
+		} elseif( $cap == 'read_' . $post_type . '_post' ) {
+			$post = get_post( $args[0] );
+
+			if ( 'private' != $post->post_status ) {
 				$caps[0] = 'read';
-			else
-				$caps[0] = 'read_private_prospress_posts';
+			} else {
+				$author_data = get_userdata( $user_id );
+				$post_author_data = get_userdata( $post->post_author );
+				if ( is_object( $post_author_data ) && $user_id == $post_author_data->ID )
+					$caps[0] = 'read';
+				else
+					$caps[0] = 'read_private_' . $post_type . '_posts';
+			}
 		}
 	}
-
 	return $caps;
 }
-add_filter( 'map_meta_cap', 'pp_map_meta_cap', 10, 4 );
+add_filter( 'map_meta_cap', 'mc_map_meta_cap', 10, 4 );
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-=== Multisite User Management ===
+=== Map Cap ===
 Contributors: thenbrent
 Tags: capabilities, roles, custom post types
 Requires at least: 3.0
@@ -17,45 +17,20 @@ For the technically endowed, this plugin takes care of mapping meta capabilities
 
 == Installation ==
 
-Please follow these instructions carefully. The plugin uses a special Multisite so it requires a slightly different installation to your vanilla WordPress plugin. 
-
-1. Upload the `ms-user-management.php` file to the `wp-content/mu-plugins/` directory (you can discard the directory and its contents). 
-1. Once uploaded to `mu-plugins`, the plugin will be activated automatically.
-1. Navigate to the **Multisite User Management** section of the *Super Admin | Options* page and set the default role for each of your sites.
-
-Note: WordPress does not create the `mu-plugins` directory by default so you may need to create it. This plugin will not work in the `wp-content/plugins` directory. 
+1. Download and unzip the plugin
+1. Upload the `map-cap` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the `Plugins` menu in WordPress
+1. Navigate to `Map Cap` under the `Settings` menu
 
 == Frequently Asked Questions ==
 
-= Does the plugin require a Multisite installation? =
+= Why aren't all my custom post types listed on the options page? =
 
-Yes, WordPress takes care of the default role on non-Multisite installations.
-
-= Why can't I install this plugin in the wp-content/plugins directory? =
-
-WordPress accesses the standard plugins directory after a user is activated.
-
-= Why aren't all my sites listed on the options page? =
-
-Only blogs marked as public and flagged as safe (mature flag off) are included in the list.
-
-= Does this plugin assign the default role to existing users? =
-
-Yes, existing users will receive the default role. If you change the default role, existing users with the old default role will receive the new default role.
-
-= Will default roles be allocated to new users who are also registering a new site? =
-
-Yes, users registering with a site will receive all the existing default roles. 
-
-The new site will not have a default role until it is manually set. Once set, all existing users will receive that role for the new site.
-
-= Why are new users registering with a site not given the default role for the dashboard site? =
-
-This is by design in the WordPress core.
+Only post types marked as public and not built-in (eg. page) are included in the list.
 
 = Where can I make feature requests, get support & report bugs? =
 
-Submit an Issue on the plugin's [Github page](http://github.com/thenbrent/multisite-user-management/issues).
+Submit an Issue on the plugin's [Github page](http://github.com/thenbrent/map-cap/issues).
 
 == Screenshots ==
 
